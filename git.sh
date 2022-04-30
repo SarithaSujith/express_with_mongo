db7397ffa66a8c12f0197535e7a71a587665734f
Saritha Sujith <pillai.sujith.saritha@gmail.com>
2022-04-30 21:52:36 +0200

    first commit

--- git.sh
+++ git.sh
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/SarithaSujith/express_with_mongodb.git
+git remote add origin https://github.com/SarithaSujith/express_with_mongo.git
 git push -u origin main
